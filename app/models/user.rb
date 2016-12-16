53af0ed5d1960588895c2ff82e3b1060e08c84d7
Elliot Brock <ell_304@msn.com>
2016-12-16 00:59:37 +0000

    finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :gardens
-  has_many :images, through: :gardens
+  # has_many :images, through: :gardens
   has_many :comments
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
--- app/serializers/image_serializer.rb
+++ app/serializers/image_serializer.rb
@@ -1,5 +1,4 @@
 class ImageSerializer < ActiveModel::Serializer
   attributes :id, :url
   has_one :garden
-  has_one :user, through: :garden
 end
